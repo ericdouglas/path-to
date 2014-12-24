95805aafd0003d3be6c58527369fc0a7acab5462
ericdouglas <eric.eoop@gmail.com>
2014-12-24 15:25:15 -0200

    test if our module return the expected value when called withou a file/folder name

--- test/index-test.js
+++ test/index-test.js
@@ -5,4 +5,8 @@ describe( '#pathTo', function() {
   it( 'create a 5lvl path to app.js file', function() {
     expect( path( 5, 'app' )).to.equal( '../../../../../app' );
   });
+
+  it( 'create a 3lvl path without a file or folder name', function() {
+    expect( path( 3 )).to.equal( '../../../' );
+  });
 });
