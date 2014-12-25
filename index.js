986aca049adda1354ee7e67faa351d55998e4e7e
Kaique da Silva <kaique.developer@gmail.com>
2014-12-25 01:42:06 -0200

    Fixed args Fixed tests

--- index.js
+++ index.js
@@ -7,10 +7,15 @@
 function pathToHandler(level, archive) {
   var path = require('path');
   var out = null;
-  
-  level = level ? new Array(level + 1).join('../') : '../';
-  archive = archive || '';
-  out = path.resolve(level, archive);
+  var args = Array.prototype.slice.call(arguments, []);
+  var hasTwoArgs = (args.length === 2);
+  var hasFirstArgAsNumber = (args.length === 1 && (typeof(level) === 'number'));
+
+  if (hasTwoArgs || (hasFirstArgAsNumber)) {
+    level = level ? new Array(level + 1).join('../') : '../';
+    archive = archive || '';
+    out = path.resolve(level, archive);
+  }
   
   return out
 }
