48a15b4a62bb172a1451175c7e32908f864d94a3
ericdouglas <eric.eoop@gmail.com>
2014-12-25 07:37:01 -0200

    fix index.js identation

--- index.js
+++ index.js
@@ -4,19 +4,21 @@
 * @param {Number} level
 * @param {String} archive
 */
-function pathToHandler(level, archive) {
-  var path = require('path');
-  var out = null;
-  var args = Array.prototype.slice.call(arguments, []);
-  var hasTwoArgs = (args.length === 2);
-  var hasFirstArgAsNumber = (args.length === 1 && (typeof(level) === 'number'));
 
-  if (hasTwoArgs || (hasFirstArgAsNumber)) {
-    level = level ? new Array(level + 1).join('../') : '../';
+function pathToHandler(level, archive) {
+  var path = require( 'path' );
+  var out = null;
+  var args = Array.prototype.slice.call( arguments, [] );
+  var hasTwoArgs = ( args.length === 2 );
+  var hasFirstArgAsNumber = ( args.length === 1 && ( typeof( level ) === 'number' ));
+
+  if ( hasTwoArgs || ( hasFirstArgAsNumber )) {
+    level = level ? new Array( level + 1 ).join( '../' ) : '../';
     archive = archive || '';
-    out = path.resolve(level, archive);
+    out = path.resolve( level, archive );
   }
   
   return out;
 }
+
 module.exports = pathToHandler;
